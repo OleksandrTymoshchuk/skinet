49ba5835db98ad1fe2642811be39703ad62b0376
Oleksandr Tymoshchuk <oleksandrtymoshchuk22@gmail.com>
2020-08-05 22:04:35 +0300

    Implemented retrieving brands and types on client

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,3 +1,5 @@
+import { IType } from './../shared/models/productType';
+import { IBrand } from './../shared/models/brand';
 import { ShopService } from './shop.service';
 import { IProduct } from './../shared/models/product';
 import { Component, OnInit } from '@angular/core';
@@ -9,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
   constructor(private shopService: ShopService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
     this.shopService.getProducts().subscribe(responce => {
       this.products = responce.data;
     }, error => {
@@ -19,4 +29,19 @@ export class ShopComponent implements OnInit {
     });
   }
 
+  getBrands() {
+    this.shopService.getBrands().subscribe( responce => {
+      this.brands = responce;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe( responce => {
+      this.types = responce;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
